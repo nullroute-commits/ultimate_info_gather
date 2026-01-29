3dfd8d705ddf4b42e77abbd7fc62a30aaa8b473e
nullroute-commits <59403925+nullroute-commits@users.noreply.github.com>
2026-01-29 09:52:15 -0600

    Update Python version requirement to 3.9
    Signed-off-by: nullroute-commits <59403925+nullroute-commits@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Ultimate Info Gather - Main Entry Point
 
-Async Python 3.11+ system information collection framework.
+Async Python 3.9+ system information collection framework.
 """
 
 import asyncio
@@ -11,8 +11,8 @@ import sys
 from src.orchestrator import main
 
 if __name__ == '__main__':
-    if sys.version_info < (3, 11):
-        print("Error: Python 3.11 or higher is required", file=sys.stderr)
+    if sys.version_info < (3, 9):
+        print("Error: Python 3.9 or higher is required", file=sys.stderr)
         sys.exit(1)
     
     sys.exit(asyncio.run(main()))
